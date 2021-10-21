d460365a23592ecd7c1e6322bbf5f402a2e5972f
IG Ignacio Gros <ignacio@gros.es>
2021-10-21 09:28:01 +0200

    Dark mode colors revision (home page link color). By EducaMadrid - www.educa.madrid.org (Iván Moriche y Rodrigo García).

--- app/src/main/java/com/example/exereader/MainActivity.java
+++ app/src/main/java/com/example/exereader/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.exereader;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity{
     private AppBarConfiguration mAppBarConfiguration;
     private NavigationView navigationView;
 
+    /* *********** educamadrid ********** */
+    private boolean esModoNoche;
+    /* *********** educamadrid ********** */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +36,8 @@ public class MainActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+
+        esModoNoche = comprobarModoNoche(getApplicationContext());
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_ayuda,R.id.nav_sugerencias,R.id.nav_creditos)
@@ -75,12 +85,39 @@ public class MainActivity extends AppCompatActivity{
 
     /* Método para controlar el estado de los item del menú */
     public void activarMenu(){
+        ColorStateList listColor = null;
+        ColorStateList listColorIcon = null;
+        if (esModoNoche) {
+            // crear lista de colores en blanco para modo oscuro
+            int[][] state = new int[][] {
+                    new int[] {-android.R.attr.state_enabled}, // disabled
+                    new int[] {android.R.attr.state_enabled}, // enabled
+                    new int[] {-android.R.attr.state_checked}, // unchecked
+                    new int[] { android.R.attr.state_pressed}  // pressed
+            };
+
+            int[] color = new int[] {
+                    Color.GRAY,
+                    Color.WHITE     ,
+                    Color.WHITE,
+                    Color.WHITE
+            };
+            listColor = new ColorStateList(state, color);
+            listColorIcon = new ColorStateList(state, color);
+        }
+
         if(navigationView != null){
             Menu menuNav=navigationView.getMenu();
             MenuItem info = menuNav.findItem(R.id.nav_gallery);
             MenuItem opc = menuNav.findItem(R.id.nav_slideshow);
             info.setEnabled(false);
             opc.setEnabled(false);
+
+            // colocar lista de colores para que no coja los colores por defecto de la app
+            if (esModoNoche && listColor != null && listColorIcon != null) {
+                navigationView.setItemTextColor(listColor);
+                navigationView.setItemIconTintList(listColorIcon);
+            }
 
             String dire = ClaseSharedPreferences.verDatos(this, "cambio");
             if(dire.equalsIgnoreCase("si")){
@@ -89,4 +126,24 @@ public class MainActivity extends AppCompatActivity{
             }
         }
     }
+
+    /* *********** educamadrid ********** */
+    /** Método para comprobar qué modo está activado en el dispositivo **/
+    public boolean comprobarModoNoche(Context context) {
+        boolean esModoNoche = false;
+
+        Configuration configuration = context.getResources().getConfiguration();
+
+        int currentNightMode = configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                //modo oscuro
+                esModoNoche = true;
+                break;
+        }
+
+        return esModoNoche;
+    }
+    /* *********** educamadrid ********** */
+
 }
